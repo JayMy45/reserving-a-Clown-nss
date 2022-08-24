5969cea1ff2a02a0a5d0ede0b931669ca22dd65f
Jeremy Myers <echelonical@gmail.com>
2022-08-24 14:22:28 -0500

    ServiceForm website code functionally renders website. Updates to dataAccess.js file with comments and new saveReques function to POST updates to database.json file

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,3 +1,7 @@
+//declare/define applications state with both requests and completions arrays [empty] to hold/save state of applications
+//fetch information from API/database.json file via fetchFunctions.
+//Post information to server/database.json file using send functions.
+
 const mainContainer = document.querySelector("#container") //added so that sendRequest would work...will have to observe how it works!
 
 const applicationState = {
@@ -9,6 +13,10 @@ const applicationState = {
 // 
 const API = "http://localhost:8088"  //api is equal to the local host that is active in Terminal.  This is where Json will look for the database.
 
+
+//Requests
+
+//? - fetchRequest function will access requests located in API then store it in applicationState function above with the key of request:   .
 export const fetchRequests = () => {
     return fetch(`${API}/requests`)
         .then(response => response.json())
@@ -18,4 +26,21 @@ export const fetchRequests = () => {
                 applicationState.requests = serviceRequests
             }
         )
+}
+
+//*** saveRequest will take userInputs supplied in userRequest parameter and POST it to Server/database.json file. Then dispatch the stateChange to the main.js file to re-render the html with updated information.
+export const saveRequest = (userRequest) => {
+    const fetchOptions = {
+        method: "Post"
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userRequest)
+    }
+    //returns the new information form the updated database.json and re-renders html using stateChanged eventListner.
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
 }
